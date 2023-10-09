e5b11b8c679e8d29a96e627a62f50096f1c17164
Arthur <119665436+atiradentes@users.noreply.github.com>
2023-10-09 16:49:10 -0300

    Initial Commit

---
+++ BrainTraining/BrainTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainingApp.swift
+//  BrainTraining
+//
+//  Created by Arthur Tiradentes on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
